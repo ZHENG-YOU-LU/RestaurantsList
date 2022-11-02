a8c493d77f2fee12dc37422ad5f2c77c9d940165
ZHENG-YOU-LU <yowyow222@gmail.com>
2022-11-02 10:58:37 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override') // 載入 method-override
 const routes = require('./routes') // 引用路由器
+const usePassport = require('./config/passport')
 
 require('./config/mongoose')
 const app = express()
+const PORT = 3000
 
 app.engine('handlebars' ,exphbs({ defaultLayout:'main' }))
 app.set('view engine', 'handlebars')
@@ -16,11 +19,19 @@ app.use(express.static('public'))
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(session({
+	secret: 'ThisIsMySecret',
+	resave: false,
+	saveUninitialized: true
+}))
+
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 
+usePassport(app) //這條要寫在 路由 app.use(routes) 之前
+
 app.use(routes) // 將 request 導入路由器
 
-app.listen(3000 ,() => {
-	console.log(`Express is running on http://localhost:${3000}`)
+app.listen(PORT ,() => {
+	console.log(`Express is running on http://localhost:${PORT}`)
 })
